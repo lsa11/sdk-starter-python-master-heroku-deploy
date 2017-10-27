f5475fb559d858cde49299578088080a0bd87233
lsa11 <leesouthwood@lees-imac.lan>
2017-10-27 12:25:31 -0400

    Bug

--- server.py
+++ server.py
@@ -173,5 +173,6 @@ def static_file(path):
     return app.send_static_file(path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
